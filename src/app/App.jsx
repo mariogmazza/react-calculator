919ee2de3efeb61cdb783eb6653b4cc137e649fa
mariogmazza <mariomazza01@gmail.com>
2018-06-02 01:58:41 -0400

    background pic added and some more css

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -5,9 +5,15 @@ import Calculator from '../Components/Calculator';
 class App extends Component {
   render() {
     return (
+      <React.Fragment>
+        <h1>A Simple ReactJS Calculator</h1>
+        <br/>
       <div className="App-container">
      <Calculator />
      </div>
+     <h4>by Mario Mazza</h4>
+     </React.Fragment>
+     
     );
   }
 }
